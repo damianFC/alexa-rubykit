d021855d3675c9847f25245a2fe2490e33e40b56
Damian Finol <damian.finol@gmail.com>
2015-04-25 12:22:16 -0700

    Test deployment

--- bin/alexa_rubyengine.rb
+++ bin/alexa_rubyengine.rb
@@ -10,10 +10,10 @@ post '/' do
   # Check that it's a valid Alexa request
   request_json = JSON.parse(request.body.read.to_s)
   halt 500 if request_json['session'].nil? || request_json['version'].nil? || request_json['request'].nil?
-
-  request = AlexaRubykit::Request.new(request_json['request'])
-  request.version = '1.0'
-  request.should_end_session = true
-  request.say_response('Hello, this is a test')
-  request.build_response
+  
+ # request = AlexaRubykit::Request.new(request_json['request'])
+ # request.version = '1.0'
+ # request.should_end_session = true
+ # request.say_response('Hello, this is a test')
+ # request.build_response
 end
