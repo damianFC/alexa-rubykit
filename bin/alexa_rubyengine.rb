0587a0ce127a5503276d91220aea6258aa7c2477
Damian Finol <damian.finol@gmail.com>
2015-04-25 12:46:14 -0700

    Testing

--- bin/alexa_rubyengine.rb
+++ bin/alexa_rubyengine.rb
@@ -11,9 +11,12 @@ post '/' do
   request_json = JSON.parse(request.body.read.to_s)
   halt 500 if request_json['session'].nil? || request_json['version'].nil? || request_json['request'].nil?
 
- request = AlexaRubykit::Request.new(request_json['request'])
- request.version = '1.0'
- request.shouldEndSession = true
- request.say_response('Hello, this is a test')
- request.build_response
+  p "1"
+  request = AlexaRubykit::Request.new(request_json['request'])
+  p "2"
+  request.version = '1.0'
+  p "3"
+  request.shouldEndSession = true
+  request.say_response('Hello, this is a test')
+  request.build_response
 end
