db0bcb0a551ce8924f7f08c17058a658bdf8a159
Damian Finol <damian.finol@gmail.com>
2015-05-02 10:06:40 -0700

    Added Alexa validation

--- lib/alexa_rubykit.rb
+++ lib/alexa_rubykit.rb
@@ -8,4 +8,9 @@ module AlexaRubykit
   def self.print_version
     p AlexaRubykit::VERSION
   end
+
+  # Returns true if all the Alexa request objects are set.
+  def self.valid_alexa?(request_json)
+    request_json['session'].nil? || request_json['version'].nil? || request_json['request'].nil?
+  end
 end
