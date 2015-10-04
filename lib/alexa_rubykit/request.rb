90b089807adef2bc1c794096e2320f5232085b9c
Alex V <email@data.bg>
2015-10-04 15:30:55 -0400

    get rid of refine for ruby19 compatibility

--- lib/alexa_rubykit/request.rb
+++ lib/alexa_rubykit/request.rb
@@ -34,13 +34,10 @@ module AlexaRubykit
     @request
   end
 
-  # Let's monkey patch Hash.
-  refine Hash do
-    # Take keys of hash and transform those to a symbols
-    def self.transform_keys_to_symbols(value)
-      return value if not value.is_a?(Hash)
-      hash = value.inject({}){|memo,(k,v)| memo[k.to_sym] = Hash.transform_keys_to_symbols(v); memo}
-      return hash
-    end
+  # Take keys of hash and transform those to a symbols
+  def self.transform_keys_to_symbols(value)
+    return value if not value.is_a?(Hash)
+    hash = value.inject({}){|memo,(k,v)| memo[k.to_sym] = self.transform_keys_to_symbols(v); memo}
+    return hash
   end
 end
